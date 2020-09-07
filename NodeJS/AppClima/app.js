38c16de294cff6855894bdf586a2af8e53c3990e
BurisMateo <burismateo@gmail.com>
2020-09-07 01:20:32 -0300

    AppClima

--- NodeJS/AppClima/app.js
+++ NodeJS/AppClima/app.js
@@ -1,3 +1,5 @@
+const { default: Axios } = require('axios');
+
 const argv = require('yargs').option({
     direccion: {
         alias: 'd',
@@ -7,3 +9,18 @@ const argv = require('yargs').option({
 }).argv;
 
 console.log(argv.direccion);
+
+const instance = Axios.create({
+    baseURL: 'https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=Colón',
+    header: {'x-rapidapi-key':'547c1c907bmsh3d1a717b24345afp12df25jsn4af2ce88b8fb'}
+});
+
+instance.get()
+    .then( resp => {
+        console.log(resp.data2);
+    })
+    .catch( err => {
+        console.log('Error!!', err);
+    });
+
+    
