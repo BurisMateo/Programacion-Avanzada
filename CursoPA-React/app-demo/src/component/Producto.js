55691d60410d73fe653b41d171ce80f6fcb3a790
BurisMateo <burismateo@gmail.com>
2020-09-26 17:29:55 -0300

    jas

--- CursoPA-React/app-demo/src/component/Producto.js
+++ CursoPA-React/app-demo/src/component/Producto.js
@@ -6,16 +6,19 @@ const Producto = ({producto, carrito, agregarProducto, productos}) => {
     //Agregar remeras al carrito
     const seleccionarProducto = () => {
         const producto = productos.filter(producto => producto.id==id)[0];
-        console.log(producto);
+        agregarProducto([
+            ...carrito,
+            producto
+        ])
     }
 
     return(
         <div>
-        <h1>{nombre}</h1>
-        <h2>{precio}</h2>
-        <button type="button" onClick={ () => seleccionarProducto()}>
-            Comprar
-        </button>
+            <h1>{nombre}</h1>
+            <h2>{precio}</h2>
+            <button type="button" onClick={ () => seleccionarProducto()}>
+                Comprar
+            </button>
         </div>
     )
 }
